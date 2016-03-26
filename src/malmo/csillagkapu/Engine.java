4457590f5c2c430061d110c9c0769b693b457923
mosomate <mosomate@gmail.com>
2016-03-26 16:40:16 +0100

    Elso commit

--- src/malmo/csillagkapu/Engine.java
+++ src/malmo/csillagkapu/Engine.java
@@ -4,4 +4,36 @@ package malmo.csillagkapu;
  * Created by Győző on 2016. 03. 26..
  */
 public class Engine {
+	private Colonel colonel;
+	private Field fields[][];
+	
+	public Engine() {
+	}
+	
+	public void init() {
+		// Belepo loggolas:
+		beginFunction();
+		
+		Loader loader = new Loader();
+		fields = loader.loadGame("palya.txt");
+		colonel = loader.getColonel();
+		
+		// Visszatero loggolas
+		endFunction("");
+	}
+	
+	public Field getField(Coordinates co) {
+		Logger.beginFunction();
+		return ret(fields[co.getX()][co.getY()]);
+	}
+	
+	public void gameOver() {
+		beginFunction();
+		endFunction("");
+	}
+	
+	public void shoot(Color c) {
+		beginFunction();
+		endFunction("");
+	}
 }
