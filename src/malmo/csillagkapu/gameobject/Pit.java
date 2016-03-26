8b660d70e71e42b55f2eb3a7a54f8372b1e0f9c0
creaTaker91 <kamumailwebre@gmail.com>
2016-03-26 17:39:15 +0100

    Add comment to Box, Pit, ZPM

--- src/malmo/csillagkapu/gameobject/Pit.java
+++ src/malmo/csillagkapu/gameobject/Pit.java
@@ -2,22 +2,30 @@ package malmo.csillagkapu.gameobject;
 
 import static malmo.csillagkapu.util.Logger.*;
 
+/**
+ * Szakadekot megvalosito osztaly
+ */
+
 public class Pit extends LandObject {
 	
+	// Constructor
 	public Pit(){ }
 	
+	// False-al jelezzuk, hogy at lehet loni rajta
 	@Override
 	public boolean hit(Bullet bul){
 		beginFunction();
 		return ret(false);
 	}
 	
+	// Jelezzuk az ezredesnek, hogy le lehet helyezni a targyat
 	@Override
 	public boolean place(ItemObject object){
 		beginFunction();
 		return ret(true);
 	}
 	
+	// Jelezzuk, hogy ra lehet lepni es megoljuk az ezredest
 	@Override
 	public boolean stepIn(Colonel col){
 		beginFunction();
--- src/malmo/csillagkapu/gameobject/ZPM.java
+++ src/malmo/csillagkapu/gameobject/ZPM.java
@@ -2,37 +2,47 @@ package malmo.csillagkapu.gameobject;
 
 import static malmo.csillagkapu.util.Logger.*;
 
+/**
+ * ZPM modult megvalosito osztaly
+ */
+
 public class ZPM extends ItemObject {
 	
+	//Meg felveheto ZPM modulok szama
 	public static int zpmCount = 0;
 	
+	// Constructor, letrehozasnal noveljuk a szamlalot 1-el
 	public ZPM(){
 		zpmCount++;
 	}
 	
+	// False-al jelezzuk, hogy at lehet loni rajta
 	@Override
 	public boolean hit(Bullet bul){
 		beginFunction();
 		return ret(false);
 	}
 	
+	// False-al jelezzuk, hogy nem lehet masik targyat lerakni
 	@Override
 	public boolean place(ItemObject object){
 		beginFunction();
 		return ret(false);
 	}
 	
+	// ZPM felvetele (szamlalot csokkentjuk 1-el)
 	@Override
 	public boolean pick(Colonel col){
 		beginFunction();
+		zpmCount++;
 		return ret(true);
 	}
 	
+	// ZPM-et tartalmazo mezore lepve felvesszuk a ZPM-et (szamlalot csokkentjuk 1-el)
 	@Override
 	public boolean stepIn(Colonel col){
 		beginFunction();
 		zpmCount++;
-		
 		return ret(false);
 	}
 }
