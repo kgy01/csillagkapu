dda2a9c5962c49327a3cefff916074b1e43cd41d
mosomate <mosomate@gmail.com>
2016-03-26 19:08:36 +0100

    new file:   src/malmo/csillagkapu/Main.java

---
+++ src/malmo/csillagkapu/Main.java
@@ -0,0 +1,10 @@
+package malmo.csillagkapu;
+
+import malmo.csillagkapu.*;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Elindult");
+		Engine = new Engine();
+	}
+}
