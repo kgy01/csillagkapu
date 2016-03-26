8ebdd9dccaa6bdc2dcd8508d214bf38692d297ab
malkowskiaron <malkowski.aron@gmail.com>
2016-03-26 18:25:30 +0100

    végleges Wall+SpecialWall

--- src/malmo/csillagkapu/gameobject/SpecialWall.java
+++ src/malmo/csillagkapu/gameobject/SpecialWall.java
@@ -2,41 +2,12 @@ package malmo.csillagkapu.gameobject;
 
 import static malmo.csillagkapu.util.Logger.*;
 
-class Wall extends Wall{
-	
-	@Override
-	boolean stepIn (Colonel col){		
-		beginFunction();
-		return false;		
-	}
-	
-	@Override
-	boolean stepOut (Colonel col){		
-		beginFunction();
-		return false;		
-	}
-	
-	@Override
-	boolean place (ItemObject obj){		
-		beginFunction();
-		return false;		
-	}
-	
-	@Override
-	boolean openPortal (Portal portal){		
-		beginFunction();
-		return false;		
-	}
-	
-	@Override
-	boolean pick (Colonel col){		
-		beginFunction();
-		return false;		
-	}
-	
+class SpecialWall extends Wall{
+
+
 	@Override
 	boolean hit (Bullet bul){		
 		beginFunction();
-		return true;		
+		return Logger.ret(true);		
 	}
 }
--- src/malmo/csillagkapu/gameobject/Wall.java
+++ src/malmo/csillagkapu/gameobject/Wall.java
@@ -3,40 +3,26 @@ package malmo.csillagkapu.gameobject;
 import static malmo.csillagkapu.util.Logger.*;
 
 class Wall extends LandObject{
+//A Pick(), OpenPortal(), Place() függvények nem változtak, ezért ezek nem kerültek be az osztályba
 	
+	//False-al térünk vissza, ugyanis a kapitány nem léphet bele a falba
 	@Override
 	boolean stepIn (Colonel col){		
 		beginFunction();
-		return false;		
+		return Logger.ret(false);		
 	}
-	
+
+	//False-al térünk vissza, ugyanis a kapitány nem léphet ki a falból(már belépni sem léphetett be)	
 	@Override
 	boolean stepOut (Colonel col){		
 		beginFunction();
-		return false;		
+		return Logger.ret(false);		
 	}
 	
-	@Override
-	boolean place (ItemObject obj){	
-		beginFunction();
-		return false;		
-	}
-	
-	@Override
-	boolean openPortal (Portal portal){
-		beginFunction();
-		return false;		
-	}
-	
-	@Override
-	boolean pick (Colonel col){		
-		beginFunction();
-		return false;	
-	}
-	
+	//False-al térünk vissza, ugyanis a nem lehet rajta keresztül lõni
 	@Override
 	boolean hit (Bullet bul){		
 		beginFunction();
-		return false;		
+		return Logger.ret(false);		
 	}
 }
