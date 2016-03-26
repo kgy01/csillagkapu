401e562a66a71d917cdd7aec066418bacf777b6b
Komporály Győző <komporaly.gyozo@gmail.com>
2016-03-26 14:51:34 +0100

    add Coordinates class, Direction enum

---
+++ src/malmo/csillagkapu/util/Coordinates.java
@@ -0,0 +1,30 @@
+package malmo.csillagkapu.util;
+
+/**
+ * Created by Győző on 2016. 03. 26..
+ */
+public class Coordinates {
+    int x;
+    int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
---
+++ src/malmo/csillagkapu/util/Direction.java
@@ -0,0 +1,8 @@
+package malmo.csillagkapu.util;
+
+/**
+ * Created by Győző on 2016. 03. 26..
+ */
+public enum Direction {
+    NORTH, EAST, WEST, SOUTH
+}
