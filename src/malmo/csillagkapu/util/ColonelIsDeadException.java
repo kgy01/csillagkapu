23f10253560ffbdb68294c4c4787a6fc20e7bc9b
Komporály Győző <komporaly.gyozo@gmail.com>
2016-04-23 14:51:19 +0200

    remove src, move src2 to src

--- src/malmo/csillagkapu/util/ColonelIsDeadException.java
+++
@@ -1,7 +0,0 @@
-package malmo.csillagkapu.util;
-
-/**
- * Created by Győző on 2016. 03. 26..
- */
-public class ColonelIsDeadException extends Exception{
-}
--- src/malmo/csillagkapu/util/PortalColor.java
+++
@@ -1,8 +0,0 @@
-package malmo.csillagkapu.util;
-
-/**
- * Created by Győző on 2016. 03. 26..
- */
-public enum PortalColor {
-    YELLOW,BLUE
-}
